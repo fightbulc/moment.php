e961bddac8ef22c9f6f243ef048f7e6d2067e3ba
Tino Ehrich <ehrich@efides.com>
2012-09-30 23:43:33 +0200

    initial commit

---
+++ Moment.php
@@ -0,0 +1,183 @@
+<?php
+
+  /**
+   * Wrapper for PHP's DateTime class inspired by moment.js
+   *
+   * @author  Tino Ehrich <ehrich@efides.com>
+   * @version 0.1
+   *
+   * @dependencies  >= PHP 5.3.0
+   *
+   */
+  class Moment extends \DateTime
+  {
+    /**
+     * @param string $dateTime
+     * @param string $timezone
+     */
+    public function __construct($dateTime = 'now', $timezone = 'UTC')
+    {
+      parent::__construct($dateTime, new DateTimeZone($timezone));
+
+      return $this;
+    }
+
+    // ##########################################
+
+    /**
+     * @param DateTimeZone $timezone
+     * @return DateTime|Moment
+     */
+    public function setTimezone($timezone)
+    {
+      parent::setTimezone(new DateTimeZone($timezone));
+
+      return $this;
+    }
+
+    // ##########################################
+
+    /**
+     * @param string $dateTime
+     * @param string $timezone
+     * @return Moment
+     */
+    public function resetDateTime($dateTime = 'now', $timezone = 'UTC')
+    {
+      parent::__construct($dateTime, new DateTimeZone($timezone));
+
+      return $this;
+    }
+
+    // ##########################################
+
+    /**
+     * @param null $format
+     * @return string
+     */
+    public function format($format = NULL)
+    {
+      if($format === NULL)
+      {
+        $format = DateTime::ISO8601;
+      }
+
+      return parent::format($format);
+    }
+
+    // ############################################
+
+    /**
+     * @param string $type
+     * @param int $value
+     * @return Moment
+     */
+    public function add($type = 'day', $value = 1)
+    {
+      parent::modify('+' . $value . ' ' . $type);
+
+      return $this;
+    }
+
+    // ############################################
+
+    /**
+     * @param string $type
+     * @param int $value
+     * @return Moment
+     */
+    public function subtract($type = 'day', $value = 1)
+    {
+      parent::modify('-' . $value . ' ' . $type);
+
+      return $this;
+    }
+
+    // ############################################
+
+    /**
+     * @param string $dateTime
+     * @param string $timezone
+     * @return array
+     */
+    public function from($dateTime = 'now', $timezone = 'UTC')
+    {
+      $fromInstance = parent::diff(new Moment($dateTime, $timezone));
+
+      $direction = $fromInstance->format('%R');
+
+      return array(
+        'seconds' => $direction . $this->_fromToSeconds($fromInstance),
+        'minutes' => $direction . round($this->_fromToMinutes($fromInstance), 2),
+        'hours'   => $direction . round($this->_fromToHours($fromInstance), 2),
+        'days'    => $direction . round($this->_fromToDays($fromInstance), 2),
+        'weeks'   => $direction . round($this->_fromToWeeks($fromInstance), 2),
+      );
+    }
+
+    // ############################################
+
+    /**
+     * @param string $timezone
+     * @return array
+     */
+    public function fromNow($timezone = 'UTC')
+    {
+      return $this->from('now', $timezone);
+    }
+
+    // ############################################
+
+    /**
+     * @param DateInterval $dateInterval
+     * @return string
+     */
+    protected function _fromToSeconds(\DateInterval $dateInterval)
+    {
+      return ($dateInterval->y * 365 * 24 * 60 * 60) + ($dateInterval->m * 30 * 24 * 60 * 60) + ($dateInterval->d * 24 * 60 * 60) + ($dateInterval->h * 60 * 60) + $dateInterval->s;
+    }
+
+    // ############################################
+
+    /**
+     * @param DateInterval $dateInterval
+     * @return string
+     */
+    protected function _fromToMinutes(\DateInterval $dateInterval)
+    {
+      return $this->_fromToSeconds($dateInterval) / 60;
+    }
+
+    // ############################################
+
+    /**
+     * @param DateInterval $dateInterval
+     * @return string
+     */
+    protected function _fromToHours(\DateInterval $dateInterval)
+    {
+      return $this->_fromToMinutes($dateInterval) / 60;
+    }
+
+    // ############################################
+
+    /**
+     * @param DateInterval $dateInterval
+     * @return string
+     */
+    protected function _fromToDays(\DateInterval $dateInterval)
+    {
+      return $this->_fromToHours($dateInterval) / 24;
+    }
+
+    // ############################################
+
+    /**
+     * @param DateInterval $dateInterval
+     * @return string
+     */
+    protected function _fromToWeeks(\DateInterval $dateInterval)
+    {
+      return $this->_fromToDays($dateInterval) / 7;
+    }
+  }
---
+++ test/test.php
@@ -0,0 +1,46 @@
+<?php
+
+  require('../Moment.php');
+  $response = array();
+
+  // ############################################
+
+  $m = new Moment();
+  $response['test01'] = $m->format();
+
+  $m = new Moment('2012-05-15T12:30:00', 'CET');
+  $response['test02'] = $m->add('hours', 2)->format();
+
+  $m = new Moment('2012-05-15T12:30:00', 'CET');
+  $response['test03'] = $m->subtract('days', 7)->subtract('minutes', 15)->format();
+
+  $m = new Moment('2012-05-12T15:00:00');
+  $result = $m->from('2011-09-25T10:00:00');
+  $response['test04.01'] = $result['seconds'];
+  $response['test04.02'] = $result['minutes'];
+  $response['test04.03'] = $result['hours'];
+  $response['test04.04'] = $result['days'];
+  $response['test04.05'] = $result['weeks'];
+
+  $m = new Moment('2013-02-01T07:00:00');
+  $result = $m->fromNow();
+  $response['test05.01'] = $result['seconds'];
+  $response['test05.02'] = $result['minutes'];
+  $response['test05.03'] = $result['hours'];
+  $response['test05.04'] = $result['days'];
+  $response['test05.05'] = $result['weeks'];
+
+  $m = new Moment('2012-04-25T03:00:00', 'CET');
+  $response['test06.01'] = $m->format();
+  $response['test06.02'] = $m->setTimezone('UTC')->format();
+
+  // ############################################
+
+  $tmpl = join('', file('test.html'));
+
+  foreach($response as $key => $value)
+  {
+    $tmpl = str_replace('{{' . $key . '}}', $value, $tmpl);
+  }
+
+  echo $tmpl;
