d00dfb6f10bdc510a799867fd4179cf9a780ef9e
fightbulc <ehrich@efides.com>
2014-03-30 19:57:07 +0200

    added setter/getter for MomentJs tokens

--- src/Moment/CustomFormats/MomentJs.php
+++ src/Moment/CustomFormats/MomentJs.php
@@ -6,6 +6,68 @@
 
     class MomentJs implements FormatsInterface
     {
+        /** @var array */
+        protected $_tokens = array(
+            "M"       => "n", //1 2 ... 11 12
+            "Mo"      => "nS", //1st 2nd ... 11th 12th
+            "MM"      => "m", //01 02 ... 11 12
+            "MMM"     => "M", //Jan Feb ... Nov Dec
+            "MMMM"    => "F", //January February ... November December
+            "D"       => "j", //1 2 ... 30 30
+            "Do"      => "jS", //1st 2nd ... 30th 31st
+            "DD"      => "d", //01 02 ... 30 31
+            "DDD"     => "z", //1 2 ... 364 365
+            "DDDo"    => "zS", //1st 2nd ... 364th 365th
+            "DDDD"    => "zS", // ***001 002 ... 364 365
+            "d"       => "w", //0 1 ... 5 6
+            "do"      => "wS", //0th 1st ... 5th 6th
+            "dd"      => "D", // ***Su Mo ... Fr Sa
+            "ddd"     => "D", //Sun Mon ... Fri Sat
+            "dddd"    => "l", //Sunday Monday ... Friday Saturday
+            "e"       => "w", //0 1 ... 5 6
+            "E"       => "N", //1 2 ... 6 7
+            "w"       => "W", //1 2 ... 52 53
+            "wo"      => "WS", //1st 2nd ... 52nd 53rd
+            "ww"      => "W", // ***01 02 ... 52 53
+            "W"       => "W", //1 2 ... 52 53
+            "Wo"      => "WS", //1st 2nd ... 52nd 53rd
+            "WW"      => "W", // ***01 02 ... 52 53
+            "YY"      => "y", //70 71 ... 29 30
+            "YYYY"    => "Y", //1970 1971 ... 2029 2030
+            "gg"      => "o", //70 71 ... 29 30
+            "gggg"    => "o", // ***1970 1971 ... 2029 2030
+            "GG"      => "o", //70 71 ... 29 30
+            "GGGG"    => "o", // ***1970 1971 ... 2029 2030
+            "A"       => "A", //AM PM
+            "a"       => "a", //am pm
+            "H"       => "G", //0 1 ... 22 23
+            "HH"      => "H", //00 01 ... 22 23
+            "h"       => "g", //1 2 ... 11 12
+            "hh"      => "h", //01 02 ... 11 12
+            "m"       => "i", //0 1 ... 58 59
+            "mm"      => "i", // ***00 01 ... 58 59
+            "s"       => "s", //0 1 ... 58 59
+            "ss"      => "s", // ***00 01 ... 58 59
+            "S"       => "", //0 1 ... 8 9
+            "SS"      => "", //0 1 ... 98 99
+            "SSS"     => "", //0 1 ... 998 999
+            "z or zz" => "T", //EST CST ... MST PST 
+            "Z"       => "P", //-07:00 -06:00 ... +06:00 +07:00
+            "ZZ"      => "O", //-0700 -0600 ... +0600 +0700
+            "X"       => "U", //1360013296
+            "LT"      => "g:i A", //8:30 PM
+            "L"       => "m/d/Y", //09/04/1986
+            "l"       => "n/j/Y", //9/4/1986
+            "LL"      => "F jS Y", //September 4th 1986
+            "ll"      => "M j Y", //Sep 4 1986
+            "LLL"     => "F js Y g:i A", //September 4th 1986 8:30 PM
+            "lll"     => "M j Y g:i A", //Sep 4 1986 8:30 PM
+            "LLLL"    => "l, F jS Y g:i A", //Thursday, September 4th 1986 8:30 PM
+            "llll"    => "D, M j Y g:i A", //Thu, Sep 4 1986 8:30 PM
+        );
+
+        // ############################################
+
         /**
          * @param $format
          *
@@ -19,73 +81,37 @@
         // ############################################
 
         /**
+         * @return array
+         */
+        public function getTokens()
+        {
+            return (array)$this->_tokens;
+        }
+
+        // ############################################
+
+        /**
+         * @param array $tokens
+         *
+         * @return MomentJs
+         */
+        public function setTokens(array $tokens)
+        {
+            $this->_tokens = $tokens;
+
+            return $this;
+        }
+
+        // ############################################
+
+        /**
          * @param String $format
          *
          * @return string
          */
         protected function _momentJsToPhp($format)
         {
-            //conversion chart
-            $tokens = array(
-                "M"       => "n", //1 2 ... 11 12
-                "Mo"      => "nS", //1st 2nd ... 11th 12th
-                "MM"      => "m", //01 02 ... 11 12
-                "MMM"     => "M", //Jan Feb ... Nov Dec
-                "MMMM"    => "F", //January February ... November December
-                "D"       => "j", //1 2 ... 30 30
-                "Do"      => "jS", //1st 2nd ... 30th 31st
-                "DD"      => "d", //01 02 ... 30 31
-                "DDD"     => "z", //1 2 ... 364 365
-                "DDDo"    => "zS", //1st 2nd ... 364th 365th
-                "DDDD"    => "zS", // ***001 002 ... 364 365
-                "d"       => "w", //0 1 ... 5 6
-                "do"      => "wS", //0th 1st ... 5th 6th
-                "dd"      => "D", // ***Su Mo ... Fr Sa
-                "ddd"     => "D", //Sun Mon ... Fri Sat
-                "dddd"    => "l", //Sunday Monday ... Friday Saturday
-                "e"       => "w", //0 1 ... 5 6
-                "E"       => "N", //1 2 ... 6 7
-                "w"       => "W", //1 2 ... 52 53
-                "wo"      => "WS", //1st 2nd ... 52nd 53rd
-                "ww"      => "W", // ***01 02 ... 52 53
-                "W"       => "W", //1 2 ... 52 53
-                "Wo"      => "WS", //1st 2nd ... 52nd 53rd
-                "WW"      => "W", // ***01 02 ... 52 53
-                "YY"      => "y", //70 71 ... 29 30
-                "YYYY"    => "Y", //1970 1971 ... 2029 2030
-                "gg"      => "o", //70 71 ... 29 30
-                "gggg"    => "o", // ***1970 1971 ... 2029 2030
-                "GG"      => "o", //70 71 ... 29 30
-                "GGGG"    => "o", // ***1970 1971 ... 2029 2030
-                "A"       => "A", //AM PM
-                "a"       => "a", //am pm
-                "H"       => "G", //0 1 ... 22 23
-                "HH"      => "H", //00 01 ... 22 23
-                "h"       => "g", //1 2 ... 11 12
-                "hh"      => "h", //01 02 ... 11 12
-                "m"       => "i", //0 1 ... 58 59
-                "mm"      => "i", // ***00 01 ... 58 59
-                "s"       => "s", //0 1 ... 58 59
-                "ss"      => "s", // ***00 01 ... 58 59
-                "S"       => "", //0 1 ... 8 9
-                "SS"      => "", //0 1 ... 98 99
-                "SSS"     => "", //0 1 ... 998 999
-                "z or zz" => "T", //EST CST ... MST PST 
-                "Z"       => "P", //-07:00 -06:00 ... +06:00 +07:00
-                "ZZ"      => "O", //-0700 -0600 ... +0600 +0700
-                "X"       => "U", //1360013296
-                "LT"      => "g:i A", //8:30 PM
-                "L"       => "m/d/Y", //09/04/1986
-                "l"       => "n/j/Y", //9/4/1986
-                "LL"      => "F jS Y", //September 4th 1986
-                "ll"      => "M j Y", //Sep 4 1986
-                "LLL"     => "F js Y g:i A", //September 4th 1986 8:30 PM
-                "lll"     => "M j Y g:i A", //Sep 4 1986 8:30 PM
-                "LLLL"    => "l, F jS Y g:i A", //Thursday, September 4th 1986 8:30 PM
-                "llll"    => "D, M j Y g:i A", //Thu, Sep 4 1986 8:30 PM
-            );
-
-            // ----------------------------------
+            $tokens = $this->getTokens();
 
             //find all tokens from string, using regular expression
             $regExp = "/(\[[^\[]*\])|(\\\\)?(LT|LL?L?L?|l{1,4}|Mo|MM?M?M?|Do|DDDo|DD?D?D?|ddd?d?|do?|w[o|w]?|W[o|W]?|YYYYY|YYYY|YY|gg(ggg?)?|GG(GGG?)?|e|E|a|A|hh?|HH?|mm?|ss?|SS?S?|X|zz?|ZZ?|.)/";
