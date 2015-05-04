284a068c7930485d78a4ab997b2ad6d8a839bff1
Jan Ptacek <jan.ptacek@gmail.com>
2015-05-04 09:39:43 +0200

    correct grammatical cases for cs_CZ

--- src/Locales/cs_CZ.php
+++ src/Locales/cs_CZ.php
@@ -24,23 +24,73 @@ return array(
     "relativeTime"  => array(
         "future" => 'za %s',
         "past"   => 'před %s',
-        "s"      => 'okamžikem',
-        "m"      => 'minutou',
-        "mm"     => '%d minutami',
-        "h"      => 'hodinou',
-        "hh"     => '%d hodinami',
-        "d"      => 'dnem',
-        "dd"     => '%d dny',
-        "M"      => 'měsícem',
-        "MM"     => '%d měsíci',
-        "y"      => 'rokem',
-        "yy"     => '%d roky',
+        "s" => function ($count, $direction, $m) {
+                    if ($direction == 'past')   return 'okamžikem';
+                    if ($direction == 'future') return 'okamžik';
+                },
+        "m" => function ($count, $direction, $m) {
+                    if ($direction == 'past')   return 'minutou';
+                    if ($direction == 'future') return 'minutu';
+                },
+        "mm" => function ($count, $direction, $m) {
+                    if ($direction == 'past') return '%d minutami';
+                    if ($direction == 'future') {
+                        if ($count < 5)  return '%d minuty';
+                        if ($count >= 5) return '%d minut';
+                    }
+                },
+        "h" => function ($count, $direction, $m) {
+                    if ($direction == 'past')   return 'hodinou';
+                    if ($direction == 'future') return 'hodinu';
+                },
+        "hh" => function ($count, $direction, $m) {
+                    if ($direction == 'past') return '%d hodinami';
+                    if ($direction == 'future') {
+                        if ($count < 5)  return '%d hodiny';
+                        if ($count >= 5) return '%d hodin';
+                    }
+                },
+        "d" => function ($count, $direction, $m) {
+                    if ($direction == 'past')   return 'dnem';
+                    if ($direction == 'future') return 'den';
+                },
+        "dd" => function ($count, $direction, $m) {
+                    if ($direction == 'past') return '%d dny';
+                    if ($direction == 'future') {
+                        if ($count < 5)  return '%d dny';
+                        if ($count >= 5) return '%d dnů';
+                    }
+                },
+        "M" => function ($count, $direction, $m) {
+                    if ($direction == 'past')   return 'měsícem';
+                    if ($direction == 'future') return 'měsíc';
+                },
+        "MM" => function ($count, $direction, $m) {
+                    if ($direction == 'past') return '%d měsíci';
+                    if ($direction == 'future') {
+                        if ($count < 5)  return '%d měsíce';
+                        if ($count >= 5) return '%d měsíců';
+                    }
+                },
+        "y" => function ($count, $direction, $m) {
+                    if ($direction == 'past')   return 'rokem';
+                    if ($direction == 'future') return 'rok';
+                },
+        "yy" => function ($count, $direction, $m) {
+                    if ($direction == 'past') {
+                        if ($count < 5)  return '%d roky';
+                        if ($count >= 5) return '%d lety';
+                    };
+                    if ($direction == 'future') {
+                        if ($count < 5)  return '%d roky';
+                        if ($count >= 5) return '%d let';
+                    }
+                },
     ),
-    "ordinal"       => function ($number)
-    {
+    "ordinal" => function ($number) {
         return $number . '.';
     },
-    "week"          => array(
+    "week"  => array(
         "dow" => 1, // Monday is the first day of the week.
         "doy" => 4  // The week that contains Jan 4th is the first week of the year.
     ),
