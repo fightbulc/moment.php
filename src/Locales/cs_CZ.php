3a6f5c7040c131ca75354cecb80415fe6e76374f
Tino Ehrich <ehrich@efides.com>
2015-05-04 10:51:31 +0200

    abstract logic for cz locale

--- src/Locales/cs_CZ.php
+++ src/Locales/cs_CZ.php
@@ -3,6 +3,33 @@
 // locale: Czech (Czech Republic) (cs_CZ)
 // author: Jan Ptacek https://github.com/ptica
 
+use Moment\Moment;
+
+/**
+ * @param string $direction
+ * @param string $trueString
+ * @param string $falseString
+ *
+ * @return string
+ */
+$ifPast = function ($direction, $trueString, $falseString)
+{
+    return $direction === 'past' ? $trueString : $falseString;
+};
+
+/**
+ * @param int    $count
+ * @param int    $countSmallerThan
+ * @param string $trueString
+ * @param string $falseString
+ *
+ * @return string
+ */
+$isCountSmaller = function ($count, $countSmallerThan, $trueString, $falseString)
+{
+    return $count < $countSmallerThan ? $trueString : $falseString;
+};
+
 return array(
     "months"        => explode('_', 'ledna_února_března_dubna_května_června_července_srpna_září_října_listopadu_prosince'),
     "monthsShort"   => explode('_', 'Led_Úno_Bře_Dub_Kvě_Čer_Čvc_Srp_Zář_Říj_Lis_Pro'),
@@ -17,80 +44,48 @@ return array(
         "withTime" => '[v] H:i',
         "default"  => 'd.m.Y',
     ),
-    // TODO
-    // relative times are different in Czech for past and future ones
-    // there is a different grammatical case being used
-    // so a Callable would be better here
     "relativeTime"  => array(
         "future" => 'za %s',
         "past"   => 'před %s',
-        "s" => function ($count, $direction, $m) {
-                    if ($direction == 'past')   return 'okamžikem';
-                    if ($direction == 'future') return 'okamžik';
-                },
-        "m" => function ($count, $direction, $m) {
-                    if ($direction == 'past')   return 'minutou';
-                    if ($direction == 'future') return 'minutu';
-                },
-        "mm" => function ($count, $direction, $m) {
-                    if ($direction == 'past') return '%d minutami';
-                    if ($direction == 'future') {
-                        if ($count < 5)  return '%d minuty';
-                        if ($count >= 5) return '%d minut';
-                    }
-                },
-        "h" => function ($count, $direction, $m) {
-                    if ($direction == 'past')   return 'hodinou';
-                    if ($direction == 'future') return 'hodinu';
-                },
-        "hh" => function ($count, $direction, $m) {
-                    if ($direction == 'past') return '%d hodinami';
-                    if ($direction == 'future') {
-                        if ($count < 5)  return '%d hodiny';
-                        if ($count >= 5) return '%d hodin';
-                    }
-                },
-        "d" => function ($count, $direction, $m) {
-                    if ($direction == 'past')   return 'dnem';
-                    if ($direction == 'future') return 'den';
-                },
-        "dd" => function ($count, $direction, $m) {
-                    if ($direction == 'past') return '%d dny';
-                    if ($direction == 'future') {
-                        if ($count < 5)  return '%d dny';
-                        if ($count >= 5) return '%d dnů';
-                    }
-                },
-        "M" => function ($count, $direction, $m) {
-                    if ($direction == 'past')   return 'měsícem';
-                    if ($direction == 'future') return 'měsíc';
-                },
-        "MM" => function ($count, $direction, $m) {
-                    if ($direction == 'past') return '%d měsíci';
-                    if ($direction == 'future') {
-                        if ($count < 5)  return '%d měsíce';
-                        if ($count >= 5) return '%d měsíců';
-                    }
-                },
-        "y" => function ($count, $direction, $m) {
-                    if ($direction == 'past')   return 'rokem';
-                    if ($direction == 'future') return 'rok';
-                },
-        "yy" => function ($count, $direction, $m) {
-                    if ($direction == 'past') {
-                        if ($count < 5)  return '%d roky';
-                        if ($count >= 5) return '%d lety';
-                    };
-                    if ($direction == 'future') {
-                        if ($count < 5)  return '%d roky';
-                        if ($count >= 5) return '%d let';
-                    }
-                },
+        "s"      => function ($count, $direction, Moment $m) use ($ifPast) {
+            return $ifPast($direction, 'okamžikem', 'okamžik');
+        },
+        "m"      => function ($count, $direction, Moment $m) use ($ifPast) {
+            return $ifPast($direction, 'minutou', 'minutu');
+        },
+        "mm"     => function ($count, $direction, Moment $m) use ($ifPast, $isCountSmaller) {
+            return $ifPast($direction, '%d minutami', $isCountSmaller($count, 5, '%d minuty', '%d minut'));
+        },
+        "h"      => function ($count, $direction, Moment $m) use ($ifPast, $isCountSmaller) {
+            return $ifPast($direction, 'hodinou', 'hodinu');
+        },
+        "hh"     => function ($count, $direction, Moment $m) use ($ifPast, $isCountSmaller) {
+            $ifPast($direction, '%d hodinami', $isCountSmaller($count, 5, '%d hodiny', '%d hodin'));
+        },
+        "d"      => function ($count, $direction, Moment $m) use ($ifPast) {
+            return $direction === 'past' ? 'dnem' : 'den';
+        },
+        "dd"     => function ($count, $direction, Moment $m) use ($ifPast, $isCountSmaller) {
+            return $ifPast($direction, '%d dny', $isCountSmaller($count, 5, '%d dny', '%d dnů'));
+        },
+        "M"      => function ($count, $direction, Moment $m) use ($ifPast) {
+            return $ifPast($direction, 'měsícem', 'měsíc');
+        },
+        "MM"     => function ($count, $direction, Moment $m) use ($ifPast, $isCountSmaller) {
+            return $ifPast($direction, '%d měsíci', $isCountSmaller($count, 5, '%d měsíce', '%d měsíců'));
+        },
+        "y"      => function ($count, $direction, Moment $m) use ($ifPast) {
+            return $ifPast($direction, 'rokem', 'rok');
+        },
+        "yy"     => function ($count, $direction, Moment $m) use ($ifPast, $isCountSmaller) {
+            return $ifPast($direction, $isCountSmaller($count, 5, '%d roky', '%d lety'), $isCountSmaller($count, 5, '%d roky', '%d let'));
+        },
     ),
-    "ordinal" => function ($number) {
+    "ordinal"       => function ($number)
+    {
         return $number . '.';
     },
-    "week"  => array(
+    "week"          => array(
         "dow" => 1, // Monday is the first day of the week.
         "doy" => 4  // The week that contains Jan 4th is the first week of the year.
     ),
