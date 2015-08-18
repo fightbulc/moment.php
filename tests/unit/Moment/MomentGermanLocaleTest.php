4c14a0e14de42fc64618937f71cac4770e800de7
Tino Ehrich <ehrich@efides.com>
2015-08-18 20:38:40 +0200

    added travis

--- tests/unit/Moment/MomentGermanLocaleTest.php
+++ tests/unit/Moment/MomentGermanLocaleTest.php
@@ -69,5 +69,9 @@ class MomentGermanLocaleTest extends \PHPUnit_Framework_TestCase
         $string = '2015-06-14 20:46:22';
         $moment = new Moment($string, 'Europe/Berlin');
         $this->assertEquals('14. Juni', $moment->format('d. F'));
+
+        $string = '2015-03-08T15:14:53-0500';
+        $moment = new Moment($string, 'Europe/Berlin');
+        $this->assertEquals('08. März', $moment->format('d. F'));
     }
 }
