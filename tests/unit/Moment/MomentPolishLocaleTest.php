ad142632052bfcee23a846835ebf45ed367e5d1b
Mateusz Błaszczyk <mateusz.blaszczyk@live.com>
2016-01-29 11:35:15 +0100

    fix tests

--- tests/unit/Moment/MomentPolishLocaleTest.php
+++ tests/unit/Moment/MomentPolishLocaleTest.php
@@ -33,41 +33,9 @@ class MomentPolishLocaleTest extends \PHPUnit_Framework_TestCase
             $moment->addDays(1);
         }
     }
-/*
-    public function testMonthNames()
+
+    public function testDayMonthFormat001()
     {
-        $startingDate = '2015-01-04T00:00:00+0000';
-
-        $moment = new Moment($startingDate);
-
-        $monthNames = array(
-            1  => array('Jan', 'Januar'),
-            2  => array('Feb', 'Februar'),
-            3  => array('Mär', 'März'),
-            4  => array('Apr', 'April'),
-            5  => array('Mai', 'Mai'),
-            6  => array('Jun', 'Juni'),
-            7  => array('Jul', 'Juli'),
-            8  => array('Aug', 'August'),
-            9  => array('Sep', 'September'),
-            10 => array('Okt', 'Oktober'),
-            11 => array('Nov', 'November'),
-            12 => array('Dez', 'Dezember'),
-        );
-
-        for ($d = 1; $d < 12; $d++)
-        {
-            $this->assertEquals($monthNames[$moment->format('n')][0], $moment->getMonthNameShort(), 'month short name failed');
-            $this->assertEquals($monthNames[$moment->format('n')][1], $moment->getMonthNameLong(), 'month long name failed');
-
-            $moment->addMonths(1);
-        }
-    }*/
-
-    public function testHirbodIssueLocaleDate001()
-    {
-        // @see: https://github.com/fightbulc/moment.php/issues/50
-
         $string = '2015-06-14 20:46:22';
         $moment = new Moment($string, 'Europe/Berlin');
         $this->assertEquals('14 czerwca', $moment->format('j F'));
@@ -77,10 +45,8 @@ class MomentPolishLocaleTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('8 marca', $moment->format('j F'));
     }
 
-    public function testHirbodIssueLocaleDate002()
+    public function testDayMonthFormat002()
     {
-        // @see: https://github.com/fightbulc/moment.php/issues/61
-
         $moment = new Moment('2016-01-03 16:17:07', 'Europe/Berlin');
         $this->assertEquals('3 grudnia', $moment->subtractMonths(1)->format('j F'));
     }
