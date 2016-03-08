e0ca2e2c5d6c1fddfa8547d2ccedab4b6259fabb
Tino Ehrich <tino@bigpun.me>
2016-03-08 16:14:34 +0100

    cleaned

--- src/Locales/zh_TW.php
+++ src/Locales/zh_TW.php
@@ -5,8 +5,6 @@
 // author: Senorsen https://github.com/Senorsen
 // author: Tino Ehrich https://github.com/fightbulc
 
-use Moment\Moment;
-
 return array(
     "months"        => explode('_', '一月_二月_三月_四月_五月_六月_七月_八月_九月_十月_十一月_十二月'),
     "monthsShort"   => explode('_', '1月_2月_3月_4月_5月_6月_7月_8月_9月_10月_11月_12月'),
