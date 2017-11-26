556d34f2537704b19cd17ed042f789dad63777b3
xZero707 <xzero@elite7hackers.net>
2017-11-26 23:06:35 +0100

    Conversion of several locales
    PHP5.3 is no longer supported due to usage of new shorter array syntax construct []. PHP5.4 from  now on. <PHP5.6 is obsolete and no longer supported, so killing PHP5.3 support is desired

--- src/Locales/en_GB.php
+++ src/Locales/en_GB.php
@@ -1,52 +1,80 @@
 <?php
 
-// locale: great britain english (en-gb)
-// author: Chris Gedrim https://github.com/chrisgedrim
+namespace Moment\Locales;
 
-return array(
-    "months"        => explode('_', 'January_February_March_April_May_June_July_August_September_October_November_December'),
-    "monthsNominative"        => explode('_', 'January_February_March_April_May_June_July_August_September_October_November_December'),
-    "monthsShort"   => explode('_', 'Jan_Feb_Mar_Apr_May_Jun_Jul_Aug_Sep_Oct_Nov_Dec'),
-    "weekdays"      => explode('_', 'Monday_Tuesday_Wednesday_Thursday_Friday_Saturday_Sunday'),
-    "weekdaysShort" => explode('_', 'Mon_Tue_Wed_Thu_Fri_Sat_Sun'),
-    "calendar"      => array(
-        "sameDay"  => '[Today]',
-        "nextDay"  => '[Tomorrow]',
-        "lastDay"  => '[Yesterday]',
-        "lastWeek" => '[Last] l',
-        "sameElse" => 'l',
-        "withTime" => '[at] H:i',
-        "default"  => 'd/m/Y',
-    ),
-    "relativeTime"  => array(
-        "future" => 'in %s',
-        "past"   => '%s ago',
-        "s"      => 'a few seconds',
-        "m"      => 'a minute',
-        "mm"     => '%d minutes',
-        "h"      => 'an hour',
-        "hh"     => '%d hours',
-        "d"      => 'a day',
-        "dd"     => '%d days',
-        "M"      => 'a month',
-        "MM"     => '%d months',
-        "y"      => 'a year',
-        "yy"     => '%d years',
-    ),
-    "ordinal"       => function ($number)
+use Moment\Provider\LocaleProvider;
+
+/**
+ * Class en_GB
+ * @package Moment\Locales
+ *
+ * Great britain english (en-gb) locale
+ *
+ * @author Chris Gedrim <https://github.com/chrisgedrim>
+ */
+class en_GB extends LocaleProvider
+{
+    /**
+     * en_GB constructor.
+     *
+     * @param array|null $definitions - Allows overriding or adding new definitions on the fly
+     */
+    public function __construct(array $definitions = null)
     {
-        $n = $number % 100;
-        $ends = array('th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th');
+        parent::__construct('en_GB');
 
-        if ($n >= 11 && $n <= 13)
-        {
-            return $number . '[th]';
+        $this->setDefinitions([
+            "months"           => explode('_',
+                'January_February_March_April_May_June_July_August_September_October_November_December'),
+            "monthsNominative" => explode('_',
+                'January_February_March_April_May_June_July_August_September_October_November_December'),
+            "monthsShort"      => explode('_', 'Jan_Feb_Mar_Apr_May_Jun_Jul_Aug_Sep_Oct_Nov_Dec'),
+            "weekdays"         => explode('_', 'Monday_Tuesday_Wednesday_Thursday_Friday_Saturday_Sunday'),
+            "weekdaysShort"    => explode('_', 'Mon_Tue_Wed_Thu_Fri_Sat_Sun'),
+            "calendar"         => [
+                "sameDay"  => '[Today]',
+                "nextDay"  => '[Tomorrow]',
+                "lastDay"  => '[Yesterday]',
+                "lastWeek" => '[Last] l',
+                "sameElse" => 'l',
+                "withTime" => '[at] H:i',
+                "default"  => 'd/m/Y',
+            ],
+            "relativeTime"     => [
+                "future" => 'in %s',
+                "past"   => '%s ago',
+                "s"      => 'a few seconds',
+                "m"      => 'a minute',
+                "mm"     => '%d minutes',
+                "h"      => 'an hour',
+                "hh"     => '%d hours',
+                "d"      => 'a day',
+                "dd"     => '%d days',
+                "M"      => 'a month',
+                "MM"     => '%d months',
+                "y"      => 'a year',
+                "yy"     => '%d years',
+            ],
+            "ordinal"          => function ($number) {
+                $n    = $number % 100;
+                $ends = ['th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th'];
+
+                if ($n >= 11 && $n <= 13) {
+                    return $number . '[th]';
+                }
+
+                return $number . '[' . $ends[$number % 10] . ']';
+            },
+            "week"             => [
+                "dow" => 1, // Monday is the first day of the week.
+                "doy" => 4  // The week that contains Jan 4th is the first week of the year.
+            ],
+        ]);
+
+
+        // Apply $definitions if array is supplied
+        if ($definitions !== null) {
+            $this->alterDefinitions($definitions);
         }
-
-        return $number . '[' . $ends[$number % 10] . ']';
-    },
-    "week"          => array(
-        "dow" => 1, // Monday is the first day of the week.
-        "doy" => 4  // The week that contains Jan 4th is the first week of the year.
-    ),
-);
+    }
+}
--- src/Locales/pt_BR.php
+++ src/Locales/pt_BR.php
@@ -1,45 +1,75 @@
 <?php
 
-// locale: portuguese (pt-br)
-// author: Jefferson Santos https://github.com/jefflssantos
+namespace Moment\Locales;
 
 use Moment\Moment;
+use Moment\Provider\LocaleProvider;
 
-return array(
-    "months"        => explode('_', 'janeiro_fevereiro_março_abril_maio_junho_julho_agosto_setembro_outubro_novembro_dezembro'),
-    "monthsShort"   => explode('_', 'jan._fev._mar._abr._mai._jun._jul._ago._set._out._nov._dez.'),
-    "weekdays"      => explode('_', 'segunda_terça_quarta_quinta_sexta_sábado_domingo'),
-    "weekdaysShort" => explode('_', 'seg._ter._qua._qui._sex._sáb._dom.'),
-    "calendar"      => array(
-        "sameDay"  => '[hoje]',
-        "nextDay"  => '[amanhã]',
-        "lastDay"  => '[ontem]',
-        "lastWeek" => '[útimo] l',
-        "sameElse" => 'eu',
-        "withTime" => function (Moment $moment) { return '[à' . ($moment->getHour() !== 1 ? 's' : null) . '] H:i'; },
-        "default"  => 'd/m/Y',
-    ),
-    "relativeTime"  => array(
-        "future" => 'em %s',
-        "past"   => 'há %s',
-        "s"      => 'alguns segundos',
-        "m"      => 'um minuto',
-        "mm"     => '%d minutos',
-        "h"      => 'uma hora',
-        "hh"     => '%d horas',
-        "d"      => 'um dia',
-        "dd"     => '%d dias',
-        "M"      => 'um mês',
-        "MM"     => '%d meses',
-        "y"      => 'um ano',
-        "yy"     => '%d anos',
-    ),
-    "ordinal"       => function ($number)
+/**
+ * Class pt_BR
+ * @package Moment\Locales
+ *
+ * Portuguese (pt-br) locale
+ *
+ * @author Jefferson Santos <https://github.com/jefflssantos>
+ */
+class pt_BR extends LocaleProvider
+{
+    /**
+     * pt_BR constructor.
+     *
+     * @param array|null $definitions - Allows overriding or adding new definitions on the fly
+     */
+    public function __construct(array $definitions = null)
     {
-        return $number . 'º';
-    },
-    "week"          => array(
-        "dow" => 1, // Monday is the first day of the week.
-        "doy" => 4  // The week that contains Jan 4th is the first week of the year.
-    ),
-);
+        parent::__construct('pt_BR');
+
+        $this->setDefinitions([
+            "months"        => explode('_',
+                'janeiro_fevereiro_março_abril_maio_junho_julho_agosto_setembro_outubro_novembro_dezembro'),
+            "monthsShort"   => explode('_', 'jan._fev._mar._abr._mai._jun._jul._ago._set._out._nov._dez.'),
+            "weekdays"      => explode('_', 'segunda_terça_quarta_quinta_sexta_sábado_domingo'),
+            "weekdaysShort" => explode('_', 'seg._ter._qua._qui._sex._sáb._dom.'),
+            "calendar"      => [
+                "sameDay"  => '[hoje]',
+                "nextDay"  => '[amanhã]',
+                "lastDay"  => '[ontem]',
+                "lastWeek" => '[útimo] l',
+                "sameElse" => 'eu',
+                "withTime" => function (Moment $moment) {
+                    return '[à' . ($moment->getHour() !== 1 ? 's' : null) . '] H:i';
+                },
+                "default"  => 'd/m/Y',
+            ],
+            "relativeTime"  => [
+                "future" => 'em %s',
+                "past"   => 'há %s',
+                "s"      => 'alguns segundos',
+                "m"      => 'um minuto',
+                "mm"     => '%d minutos',
+                "h"      => 'uma hora',
+                "hh"     => '%d horas',
+                "d"      => 'um dia',
+                "dd"     => '%d dias',
+                "M"      => 'um mês',
+                "MM"     => '%d meses',
+                "y"      => 'um ano',
+                "yy"     => '%d anos',
+            ],
+            "ordinal"       => function ($number) {
+                return $number . 'º';
+            },
+            "week"          => [
+                "dow" => 1, // Monday is the first day of the week.
+                "doy" => 4  // The week that contains Jan 4th is the first week of the year.
+            ],
+        ]);
+
+
+        // Apply $definitions if array is supplied
+        if ($definitions !== null) {
+            $this->alterDefinitions($definitions);
+        }
+    }
+}
+
