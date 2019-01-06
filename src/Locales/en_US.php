39eefb0cec8174064ea4924d5dd7f53241813813
Mauricio Giordano <giordano@inevent.us>
2019-01-06 19:00:26 -0200

    Adjusted custom formats for en_US

--- src/Locales/en_US.php
+++ src/Locales/en_US.php
@@ -6,5 +6,16 @@ $locale = require __DIR__ . '/en_GB.php';
 $locale['calendar']['withTime'] = '[at] h:i A';
 $locale['calendar']['default'] = 'm/d/Y';
 $locale['week']['dow'] = 7;
+$locale["customFormats"] = array(
+    "LT"   => "g:i A", // 8:30 PM
+    "L"    => "m/d/Y", // 09/04/1986
+    "l"    => "n/j/Y", // 9/4/1986
+    "LL"   => "F j, Y", // September 4, 1986
+    "ll"   => "M j, Y", // Sep 4, 1986
+    "LLL"  => "F j, Y g:i A", // September 4, 1986 8:30 PM
+    "lll"  => "M j, Y g:i A", // Sep 4, 1986 8:30 PM
+    "LLLL" => "l, F j, Y g:i A", // Thursday, September 4, 1986 8:30 PM
+    "llll" => "D, M j, Y g:i A", // Thu, Sep 4, 1986 8:30 PM
+);
 
 return $locale;
